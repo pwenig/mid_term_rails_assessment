7a0a86d28f15f54d686080d4e15962627fb3e74c
Paul Wenig <pwenig@gmail.com>
2014-07-13 19:39:09 -0600

    Final test passing

--- spec/features/wall_spec.rb
+++ spec/features/wall_spec.rb
@@ -2,14 +2,10 @@ require 'rails_helper'
 require 'capybara/rails'
 
 feature 'Wall' do
-  # before do
-  #   DatabaseCleaner.clean
-  # end
   let(:user) { create_user }
   scenario 'Users can post' do
-
-    # create_user email: "user@example.com", name: "Some User"
-    # create_user email: "reader@example.com", name: "Reader User"
+    create_user email: "user@example.com", name: "Some User"
+    create_user email: "reader@example.com", name: "Reader User"
 
     visit root_path
     fill_in "Email", with: "user@example.com"
@@ -22,13 +18,13 @@ feature 'Wall' do
     fill_in "Password", with: "password"
     click_on "Login"
 
+    fill_in 'post_body', with: "Here is my post"
+    click_on "Post"
     click_on "Add Comment"
-
     expect(page).to have_content "Add a comment"
 
     fill_in "Comment", with: "Great post!"
     click_on "Add Comment"
-
     expect(page).to have_content "Great post!"
 
   end
