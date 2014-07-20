623abcfbde208bec2256fa54e50b2dd4ad933e72
Paul Wenig <pwenig@gmail.com>
2014-07-19 20:14:43 -0600

    Added created_at to comment table

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140719224013) do
+ActiveRecord::Schema.define(version: 20140720015822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: true do |t|
-    t.text    "body"
-    t.integer "post_id"
-    t.integer "user_id"
+    t.text     "body"
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
   end
 
   create_table "posts", force: true do |t|
