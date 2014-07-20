6627f7ecca5bab1b40b3756f27a682e4170b846a
Paul Wenig <pwenig@gmail.com>
2014-07-19 20:33:00 -0600

    Added validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
 belongs_to :user
+  validates :body, :presence => {:message => "Comment cannot be blank"}
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,4 +6,9 @@ describe Comment do
     comment = Comment.new(user_id: 1, post_id: 1, body: "Great post")
     expect(comment.valid?).to eq true
   end
+
+  it "checks validations" do
+    comment = Comment.new(user_id: 1, post_id: 1, body: "")
+    expect(comment.valid?).to eq false
+  end
 end
