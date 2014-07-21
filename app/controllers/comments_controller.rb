15950bfa0cdf7fce42186237642b1ae122d01b1f
Paul Wenig <pwenig@gmail.com>
2014-07-20 22:21:29 -0600

    User can add comment to a post

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
+  end
+
+def create
+  @post = Post.find(params[:post_id])
+  @comment = Comment.new(comment_attributes)
+  @comment.post_id = @post.id
+  @comment.user_id = current_user.id
+  if @comment.save
+    redirect_to root_path
+  else
+    render :new
+  end
+end
+
+  private
+  def comment_attributes
+    params.require(:comment).permit(:body)
+  end
+end
--- spec/features/wall_spec.rb
+++ spec/features/wall_spec.rb
@@ -24,4 +24,21 @@ feature 'Wall' do
     expect(page).to have_content("I'm doing some fun things here")
   end
 
+  scenario 'User can add a comment to a post' do
+    create_user email: "user@example.com", name: "Some User"
+
+    visit root_path
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    click_on "Login"
+
+    fill_in "post_body", with: "I'm doing some fun things here"
+    click_on "Post"
+    click_on "Add Comment"
+    fill_in "comment[body]", with: "I love this post"
+    click_on "Add Comment"
+
+    expect(page).to have_content "I love this post"
+  end
+
 end
