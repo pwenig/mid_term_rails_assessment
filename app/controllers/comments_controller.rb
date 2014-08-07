17f9c1a5a7fb4e35fd72e0fd19dca3d26257f9f4
Paul Wenig <pwenig@gmail.com>
2014-08-07 10:29:31 -0600

    User can see post comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,19 @@
 class CommentsController < ApplicationController
 
   def new
-  @post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = Comment.new
   end
 
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(post_id: params[:post_id], body: params[:comment][:body])
+    if @comment.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
 
 end
--- spec/features/wall_spec.rb
+++ spec/features/wall_spec.rb
@@ -72,4 +72,20 @@ feature 'Wall' do
 
   end
 
+  scenario 'User can create a comment and see it on the homepage' do
+    create_user email: "user@example.com", name: "Some User"
+    create_user email: "reader@example.com", name: "Reader User"
+
+    visit root_path
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    click_on "Login"
+
+    fill_in "post_body", with: "I'm doing some fun things here"
+    click_on "Post"
+    click_on "Add Comment"
+    fill_in "comment[body]", with: "I totally agree!"
+    click_on "Add Comment"
+    expect(page).to have_content "I totally agree!"
+  end
 end
