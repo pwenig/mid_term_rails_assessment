31b756112b49ea813478726a1b81534d585e9728
Paul Wenig <pwenig@gmail.com>
2014-08-07 10:47:04 -0600

    Added validation with model test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,4 +7,12 @@ describe 'Comment' do
     comment = Comment.new(post_id: post.id, body: "I love this post")
     expect(comment.valid?).to eq true
   end
+
+  it "cannot creates a comment for a post without a comment" do
+    user = create_user2
+    post = Post.new(user: user, body: "Digging into some clojure.  Learning to love the parenthesis.")
+    comment = Comment.new(post_id: post.id, body: "")
+    expect(comment.valid?).to eq false
+  end
+
 end
--- spec/support/object_creation_methods.rb
+++ spec/support/object_creation_methods.rb
@@ -1,8 +1,17 @@
 def create_user(overrides = {})
   User.create!({
-    name: 'Some User',
-    email: 'user@example.com',
-    password: 'password',
-    password_confirmation: 'password'
-  }.merge(overrides))
+                 name: 'Some User',
+                 email: 'user@example.com',
+                 password: 'password',
+                 password_confirmation: 'password'
+               }.merge(overrides))
+end
+
+def create_user2(overrides = {})
+  User.create!({
+                 name: 'Another User',
+                 email: 'user2@example.com',
+                 password: 'password',
+                 password_confirmation: 'password'
+               }.merge(overrides))
 end
