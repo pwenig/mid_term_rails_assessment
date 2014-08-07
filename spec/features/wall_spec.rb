fff39158e5576b46d460e469635e333084542a92
Paul Wenig <pwenig@gmail.com>
2014-08-07 09:54:00 -0600

    User can see add comment link

--- spec/features/wall_spec.rb
+++ spec/features/wall_spec.rb
@@ -24,4 +24,21 @@ feature 'Wall' do
     expect(page).to have_content("I'm doing some fun things here")
   end
 
+  scenario 'User can see Add Comment link' do
+    create_user email: "user@example.com", name: "Some User"
+    create_user email: "reader@example.com", name: "Reader User"
+
+    visit root_path
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    click_on "Login"
+
+    fill_in "post_body", with: "I'm doing some fun things here"
+    click_on "Post"
+    expect(page).to have_content "Add Comment"
+
+  end
+
+  
+
 end
